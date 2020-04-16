d1fcc2a4ff3f5f2c55565c684ef1b83d2587387d
razorwu1994 <razor.wu1994@gmail.com>
2020-04-16 18:25:01 -0400

    v0.0.3

--- scripts/npc/9000020.js
+++ scripts/npc/9000020.js
@@ -21,98 +21,148 @@
 */
 status = -1;
 
+// var travelFrom = [777777777, 541000000];
+var travelFee = [3000, 10000, 7000];
 
-var travelFrom = [777777777, 541000000];
-var travelFee = [3000, 10000];
-
-var travelMap = [800000000, 550000000];
-var travelPlace = ["Mushroom Shrine of Japan", "Trend Zone of Malaysia"];
-var travelPlaceShort = ["Mushroom Shrine", "Metropolis"];
-var travelPlaceCountry = ["Japan", "Malaysia"];
+var travelMap = [800000000, 550000000, 540000000];
+var travelPlace = [
+  "Mushroom Shrine of Japan",
+  "Trend Zone of Malaysia",
+  "Singapore - Central Business District",
+];
+var travelPlaceShort = ["Mushroom Shrine", "Metropolis", "The Lion City"];
+var travelPlaceCountry = ["Japan", "Malaysia", "Singapore"];
 var travelAgent = ["I", "#r#p9201135##k"];
 
-var travelDescription = ["If you desire to feel the essence of Japan, there's nothing like visiting the Shrine, a Japanese cultural melting pot. Mushroom Shrine is a mythical place that serves the incomparable Mushroom God from ancient times.",
-                        "If you desire to feel the heat of the tropics on an upbeat environment, the residents of Malaysia are eager to welcome you. Also, the metropolis itself is the heart of the local economy, that place is known to always offer something to do or to visit around."];
+var travelDescription = [
+  "If you desire to feel the essence of Japan, there's nothing like visiting the Shrine, a Japanese cultural melting pot. Mushroom Shrine is a mythical place that serves the incomparable Mushroom God from ancient times.",
+  "If you desire to feel the heat of the tropics on an upbeat environment, the residents of Malaysia are eager to welcome you. Also, the metropolis itself is the heart of the local economy, that place is known to always offer something to do or to visit around.",
+  "If you would like to experience lion city and meet Fan Wen Fang, go check out there.",
+];
 
-var travelDescription2 = ["Check out the female shaman serving the Mushroom God, and I strongly recommend trying Takoyaki, Yakisoba, and other delocious food sold in the streets of Japan. Now, let's head over to #bMushroom Shrine#k, a mythical place if there ever was one.",
-                        "Once there, I strongly suggest you to schedule a visit to Kampung Village. Why? Surely you've come to know about the fantasy theme park Spooky World? No? It's simply put the greatest theme park around there, it's worth a visit! Now, let's head over to the #bTrend Zone of Malaysia#k."];
+var travelDescription2 = [
+  "Check out the female shaman serving the Mushroom God, and I strongly recommend trying Takoyaki, Yakisoba, and other delocious food sold in the streets of Japan. Now, let's head over to #bMushroom Shrine#k, a mythical place if there ever was one.",
+  "Once there, I strongly suggest you to schedule a visit to Kampung Village. Why? Surely you've come to know about the fantasy theme park Spooky World? No? It's simply put the greatest theme park around there, it's worth a visit! Now, let's head over to the #bTrend Zone of Malaysia#k.",
+  "Highly recommend spit over in public and #bUncle Police#k will find you :)",
+];
 
 var travelType;
 var travelStatus;
 
 function start() {
-    travelStatus = getTravelingStatus(cm.getPlayer().getMapId());
-    action(1,0,0);
+  travelType = -1;
+  travelStatus = getTravelingStatus(cm.getPlayer().getMapId());
+  action(1, 0, 0);
 }
 
 function getTravelingStatus(mapid) {
-    for(var i = 0; i < travelMap.length; i++) {
-        if(mapid == travelMap[i]) {
-            return i;
-        }
+  for (var i = 0; i < travelMap.length; i++) {
+    if (mapid == travelMap[i]) {
+      return i;
     }
-    
-    return -1;
+  }
+
+  return -1;
 }
 
-function getTravelType(mapid) {
-    for(var i = 0; i < travelFrom.length; i++) {
-        if(mapid == travelFrom[i]) {
-            return i;
-        }
-    }
-    
-    return 0;
-}
+// function getTravelType(mapid) {
+//   for (var i = 0; i < travelFrom.length; i++) {
+//     if (mapid == travelFrom[i]) {
+//       return i;
+//     }
+//   }
+
+//   return 0;
+// }
 
 function action(mode, type, selection) {
-    status++;
-    if(mode != 1){
-        if(mode == 0 && status == 4)
-            status -= 2;
-        else{
-            cm.dispose();
-            return;
-        }
+  status++;
+  if (mode != 1) {
+    if (mode == 0 && status == 4) status -= 2;
+    else {
+      cm.dispose();
+      return;
     }
-    
-    if (travelStatus != -1) {
-        if (status == 0) 
-            cm.sendSimple("How's the traveling? Are you enjoying it?#b\r\n#L0#Yes, I'm done with traveling. Can I go back to #m" + cm.getPlayer().peekSavedLocation("WORLDTOUR") + "#?\r\n#L1#No, I'd like to continue exploring this place.");
-        else if (status == 1) {
-            if (selection == 0) {
-                cm.sendNext("Alright. I'll take you back to where you were before the visit to Japan. If you ever feel like traveling again down the road, please let me know!");
-            } else if (selection == 1) {
-                cm.sendOk("OK. If you ever change your mind, please let me know.");
-                cm.dispose();
-            }
-        } else if (status == 2) {
-            var map = cm.getPlayer().getSavedLocation("WORLDTOUR");
-            if (map == -1) map = 104000000;
-            
-            cm.warp(map);
-            cm.dispose();
-        }
-    } else {
-        if (status == 0) {
-            travelType = getTravelType(cm.getPlayer().getMapId());
-            cm.sendNext("If you're tired of the monotonous daily life, how about getting out for a change? there's nothing quite like soaking up a new culture, learning something new by the minute! It's time for you to get out and travel. We, at the Maple Travel Agency recommend you going on a #bWorld Tour#k! Are you worried about the travel expense? You shouldn't be! We, the #bMaple Travel Agency#k, have carefully come up with a plan to let you travel for ONLY #b" + cm.numberWithCommas(travelFee[travelType]) + " mesos#k!");
-        } else if (status == 1) {
-            cm.sendSimple("We currently offer this place for you traveling pleasure: #b" + travelPlace[travelType] + "#k. " + travelAgent[travelType] + "'ll be there serving you as the travel guide. Rest assured, the number of destinations will be increase over time. Now, would you like to head over to the " + travelPlaceShort[travelType] + "?#b\r\n#L0#Yes, take me to " + travelPlaceShort[travelType] + " (" + travelPlaceCountry[travelType] + ")");
-        } else if (status == 2) {
-            cm.sendNext("Would you like to travel to #b" + travelPlace[travelType] + "#k? " + travelDescription[travelType]);
-        } else if (status == 3) {
-            if(cm.getMeso() < travelFee[travelType]){
-                cm.sendNext("You don't have enough mesos to take the travel.");
-                cm.dispose();
-                return;
-            }
-            cm.sendNextPrev(travelDescription2[travelType]);
-        } else if (status == 4) {
-            cm.gainMeso(-travelFee[travelType]);
-            cm.getPlayer().saveLocation("WORLDTOUR");
-            cm.warp(travelMap[travelType], 0);
-            cm.dispose();
-        }
+  }
+
+  if (travelStatus != -1) {
+    if (status == 0)
+      cm.sendSimple(
+        "How's the traveling? Are you enjoying it?#b\r\n#L0#Yes, I'm done with traveling. Can I go back to #m" +
+          cm.getPlayer().peekSavedLocation("WORLDTOUR") +
+          "#?\r\n#L1#No, I'd like to continue exploring this place."
+      );
+    else if (status == 1) {
+      if (selection == 0) {
+        cm.sendNext(
+          "Alright. I'll take you back to where you were before the visit to Japan. If you ever feel like traveling again down the road, please let me know!"
+        );
+      } else if (selection == 1) {
+        cm.sendOk("OK. If you ever change your mind, please let me know.");
+        cm.dispose();
+      }
+    } else if (status == 2) {
+      var map = cm.getPlayer().getSavedLocation("WORLDTOUR");
+      if (map == -1) map = 104000000;
+
+      cm.warp(map);
+      cm.dispose();
     }
+  } else {
+    if (status == 0) {
+      //   travelType = getTravelType(cm.getPlayer().getMapId());
+      cm.sendNext(
+        "If you're tired of the monotonous daily life, how about getting out for a change? there's nothing quite like soaking up a new culture, learning something new by the minute! It's time for you to get out and travel. We, at the Maple Travel Agency recommend you going on a #bWorld Tour#k! Are you worried about the travel expense? You shouldn't be! We, the #bMaple Travel Agency#k, have carefully come up with a plan to let you travel "
+        // for ONLY #b" +
+        //   cm.numberWithCommas(travelFee[travelType]) +
+        //   " mesos#k!"
+      );
+    } else if (status == 1) {
+      var travelOptions = "";
+      for (var i = 0; i < travelPlace.length; i++) {
+        travelOptions +=
+          "#b\r\n#L" +
+          i +
+          "#Yes, take me to " +
+          travelPlaceShort[i] +
+          " (" +
+          travelPlaceCountry[i] +
+          ")";
+      }
+      cm.sendSimple(
+        "We currently offer this place for you traveling pleasure: #b" +
+          travelPlace[0] +
+          "#k and #b" +
+          travelPlace[1] +
+          "#k. " +
+          travelAgent[0] +
+          "'ll be there serving you as the travel guide. Rest assured, the number of destinations will be increase over time. Now, where would you like to head over " +
+          "?" +
+          travelOptions
+      );
+    } else if (status == 2) {
+      if (travelType == -1) {
+        travelType = selection;
+      }
+
+      cm.sendNext(
+        "Would you like to travel to #b" +
+          travelPlace[travelType] +
+          "#k? " +
+          travelDescription[travelType]
+      );
+    } else if (status == 3) {
+      if (cm.getMeso() < travelFee[travelType]) {
+        cm.sendNext("You don't have enough mesos to take the travel.");
+        cm.dispose();
+        return;
+      }
+      cm.sendNextPrev(travelDescription2[travelType]);
+    } else if (status == 4) {
+      cm.gainMeso(-travelFee[travelType]);
+      cm.getPlayer().saveLocation("WORLDTOUR");
+      cm.warp(travelMap[travelType], 0);
+      cm.dispose();
+    }
+  }
 }
