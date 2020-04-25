50bfcd2034c2095a7fb48cebec6f59d6894562f7
razorwu1994 <razor.wu1994@gmail.com>
2020-04-24 20:01:40 -0400

    v0.0.6beta

--- scripts/event/HenesysPQ.js
+++ scripts/event/HenesysPQ.js
@@ -21,11 +21,13 @@
 /**
  * @author: Ronan
  * @event: Henesys PQ
-*/
+ */
 
 var isPq = true;
-var minPlayers = 3, maxPlayers = 6;
-var minLevel = 10, maxLevel = 255;
+var minPlayers = 2,
+  maxPlayers = 6;
+var minLevel = 10,
+  maxLevel = 255;
 var entryMap = 910010000;
 var exitMap = 910010300;
 var recruitMap = 100000200;
@@ -34,234 +36,254 @@ var clearMap = 910010100;
 var minMapId = 910010000;
 var maxMapId = 910010400;
 
-var eventTime = 10;     // 10 minutes
+var eventTime = 10; // 10 minutes
 
 var lobbyRange = [0, 0];
 
 function init() {
-        setEventRequirements();
+  setEventRequirements();
 }
 
 function setLobbyRange() {
-        return lobbyRange;
+  return lobbyRange;
 }
 
 function setEventRequirements() {
-        var reqStr = "";
-        
-        reqStr += "\r\n    Number of players: ";
-        if(maxPlayers - minPlayers >= 1) reqStr += minPlayers + " ~ " + maxPlayers;
-        else reqStr += minPlayers;
-        
-        reqStr += "\r\n    Level range: ";
-        if(maxLevel - minLevel >= 1) reqStr += minLevel + " ~ " + maxLevel;
-        else reqStr += minLevel;
-        
-        reqStr += "\r\n    Time limit: ";
-        reqStr += eventTime + " minutes";
-        
-        em.setProperty("party", reqStr);
+  var reqStr = "";
+
+  reqStr += "\r\n    Number of players: ";
+  if (maxPlayers - minPlayers >= 1) reqStr += minPlayers + " ~ " + maxPlayers;
+  else reqStr += minPlayers;
+
+  reqStr += "\r\n    Level range: ";
+  if (maxLevel - minLevel >= 1) reqStr += minLevel + " ~ " + maxLevel;
+  else reqStr += minLevel;
+
+  reqStr += "\r\n    Time limit: ";
+  reqStr += eventTime + " minutes";
+
+  em.setProperty("party", reqStr);
 }
 
 function setEventExclusives(eim) {
-        var itemSet = [4001095, 4001096, 4001097, 4001098, 4001099, 4001100, 4001101];
-        eim.setExclusiveItems(itemSet);
+  var itemSet = [4001095, 4001096, 4001097, 4001098, 4001099, 4001100, 4001101];
+  eim.setExclusiveItems(itemSet);
 }
 
 function setEventRewards(eim) {
-        var itemSet, itemQty, evLevel, expStages;
+  var itemSet, itemQty, evLevel, expStages;
 
-        evLevel = 1;    //Rewards at clear PQ
-        itemSet = [4001158];
-        itemQty = [1];
-        eim.setEventRewards(evLevel, itemSet, itemQty);
-        
-        expStages = [1600];    //bonus exp given on CLEAR stage signal
-        eim.setEventClearStageExp(expStages);
+  evLevel = 1; //Rewards at clear PQ
+  itemSet = [4001158];
+  itemQty = [1];
+  eim.setEventRewards(evLevel, itemSet, itemQty);
+
+  expStages = [1600]; //bonus exp given on CLEAR stage signal
+  eim.setEventClearStageExp(expStages);
 }
 
-function getEligibleParty(party) {      //selects, from the given party, the team that is allowed to attempt this event
-        var eligible = [];
-        var hasLeader = false;
-        
-        if(party.size() > 0) {
-                var partyList = party.toArray();
+function getEligibleParty(party) {
+  //selects, from the given party, the team that is allowed to attempt this event
+  var eligible = [];
+  var hasLeader = false;
 
-                for(var i = 0; i < party.size(); i++) {
-                        var ch = partyList[i];
+  if (party.size() > 0) {
+    var partyList = party.toArray();
 
-                        if(ch.getMapId() == recruitMap && ch.getLevel() >= minLevel && ch.getLevel() <= maxLevel) {
-                                if(ch.isLeader()) hasLeader = true;
-                                eligible.push(ch);
-                        }
-                }
-        }
-        
-        if(!(hasLeader && eligible.length >= minPlayers && eligible.length <= maxPlayers)) eligible = [];
-        return eligible;
+    for (var i = 0; i < party.size(); i++) {
+      var ch = partyList[i];
+
+      if (
+        ch.getMapId() == recruitMap &&
+        ch.getLevel() >= minLevel &&
+        ch.getLevel() <= maxLevel
+      ) {
+        if (ch.isLeader()) hasLeader = true;
+        eligible.push(ch);
+      }
+    }
+  }
+
+  if (
+    !(
+      hasLeader &&
+      eligible.length >= minPlayers &&
+      eligible.length <= maxPlayers
+    )
+  )
+    eligible = [];
+  return eligible;
 }
 
 function setup(level, lobbyid) {
-        var eim = em.newInstance("Henesys" + lobbyid);
-        eim.setProperty("level", level);
-        eim.setProperty("stage", "0");
-        eim.setProperty("bunnyCake", "0");
-        eim.setProperty("bunnyDamaged", "0");
-        
-        eim.getInstanceMap(910010000).resetPQ(level);
-        eim.getInstanceMap(910010000).allowSummonState(false);
-        
-        eim.getInstanceMap(910010200).resetPQ(level);
-        
-        respawnStages(eim);
-        eim.startEventTimer(eventTime * 60000);
-        setEventRewards(eim);
-        setEventExclusives(eim);
-        return eim;
+  var eim = em.newInstance("Henesys" + lobbyid);
+  eim.setProperty("level", level);
+  eim.setProperty("stage", "0");
+  eim.setProperty("bunnyCake", "0");
+  eim.setProperty("bunnyDamaged", "0");
+
+  eim.getInstanceMap(910010000).resetPQ(level);
+  eim.getInstanceMap(910010000).allowSummonState(false);
+
+  eim.getInstanceMap(910010200).resetPQ(level);
+
+  respawnStages(eim);
+  eim.startEventTimer(eventTime * 60000);
+  setEventRewards(eim);
+  setEventExclusives(eim);
+  return eim;
 }
 
 function afterSetup(eim) {}
 
 function respawnStages(eim) {
-        eim.getInstanceMap(910010000).instanceMapRespawn();
-        eim.getInstanceMap(910010200).instanceMapRespawn();
-        
-        eim.schedule("respawnStages", 15 * 1000);
+  eim.getInstanceMap(910010000).instanceMapRespawn();
+  eim.getInstanceMap(910010200).instanceMapRespawn();
+
+  eim.schedule("respawnStages", 15 * 1000);
 }
 
 function playerEntry(eim, player) {
-        var map = eim.getMapInstance(entryMap);
-        player.changeMap(map, map.getPortal(0));
+  var map = eim.getMapInstance(entryMap);
+  player.changeMap(map, map.getPortal(0));
 }
 
 function scheduledTimeout(eim) {
-        if(eim.getProperty("1stageclear") != null) {
-                var curStage = 910010200, toStage = 910010400;
-                eim.warpEventTeam(curStage, toStage);
-        }
-        else {
-                end(eim);
-        }
+  if (eim.getProperty("1stageclear") != null) {
+    var curStage = 910010200,
+      toStage = 910010400;
+    eim.warpEventTeam(curStage, toStage);
+  } else {
+    end(eim);
+  }
 }
 
 function bunnyDefeated(eim) {
-        eim.dropMessage(5, "Due to your failure to protect the Moon Bunny, you have been transported to the Exile Map.");
-        end(eim);
+  eim.dropMessage(
+    5,
+    "Due to your failure to protect the Moon Bunny, you have been transported to the Exile Map."
+  );
+  end(eim);
 }
 
 function playerUnregistered(eim, player) {}
 
 function playerExit(eim, player) {
-        eim.unregisterPlayer(player);
-        player.changeMap(exitMap, 0);
+  eim.unregisterPlayer(player);
+  player.changeMap(exitMap, 0);
 }
 
 function playerLeft(eim, player) {
-        if(!eim.isEventCleared()) {
-                playerExit(eim, player);
-        }
+  if (!eim.isEventCleared()) {
+    playerExit(eim, player);
+  }
 }
 
 function changedMap(eim, player, mapid) {
-        if (mapid < minMapId || mapid > maxMapId || mapid == 910010300) {
-                if (eim.isEventTeamLackingNow(true, minPlayers, player)) {
-                        eim.unregisterPlayer(player);
-                        end(eim);
-                }
-                else
-                        eim.unregisterPlayer(player);
-        }
+  if (mapid < minMapId || mapid > maxMapId || mapid == 910010300) {
+    if (eim.isEventTeamLackingNow(true, minPlayers, player)) {
+      eim.unregisterPlayer(player);
+      end(eim);
+    } else eim.unregisterPlayer(player);
+  }
 }
 
 function changedLeader(eim, leader) {
-        var mapid = leader.getMapId();
-        if (!eim.isEventCleared() && (mapid < minMapId || mapid > maxMapId)) {
-                end(eim);
-        }
+  var mapid = leader.getMapId();
+  if (!eim.isEventCleared() && (mapid < minMapId || mapid > maxMapId)) {
+    end(eim);
+  }
 }
 
 function playerDead(eim, player) {}
 
-function playerRevive(eim, player) { // player presses ok on the death pop up.
-        if (eim.isEventTeamLackingNow(true, minPlayers, player)) {
-                eim.unregisterPlayer(player);
-                end(eim);
-        }
-        else
-                eim.unregisterPlayer(player);
+function playerRevive(eim, player) {
+  // player presses ok on the death pop up.
+  if (eim.isEventTeamLackingNow(true, minPlayers, player)) {
+    eim.unregisterPlayer(player);
+    end(eim);
+  } else eim.unregisterPlayer(player);
 }
 
 function playerDisconnected(eim, player) {
-        if (eim.isEventTeamLackingNow(true, minPlayers, player)) {
-                eim.unregisterPlayer(player);
-                end(eim);
-        }
-        else
-                eim.unregisterPlayer(player);
+  if (eim.isEventTeamLackingNow(true, minPlayers, player)) {
+    eim.unregisterPlayer(player);
+    end(eim);
+  } else eim.unregisterPlayer(player);
 }
 
 function leftParty(eim, player) {
-        if (eim.isEventTeamLackingNow(false, minPlayers, player)) {
-                end(eim);
-        }
-        else
-                playerLeft(eim, player);
+  if (eim.isEventTeamLackingNow(false, minPlayers, player)) {
+    end(eim);
+  } else playerLeft(eim, player);
 }
 
 function disbandParty(eim) {
-        if (!eim.isEventCleared()) {
-                end(eim);
-        }
+  if (!eim.isEventCleared()) {
+    end(eim);
+  }
 }
 
 function monsterValue(eim, mobId) {
-        return 1;
+  return 1;
 }
 
 function end(eim) {
-        var party = eim.getPlayers();
-        for (var i = 0; i < party.size(); i++) {
-                playerExit(eim, party.get(i));
-        }
-        eim.dispose();
+  var party = eim.getPlayers();
+  for (var i = 0; i < party.size(); i++) {
+    playerExit(eim, party.get(i));
+  }
+  eim.dispose();
 }
 
 function giveRandomEventReward(eim, player) {
-        eim.giveEventReward(player);
+  eim.giveEventReward(player);
 }
 
 function clearPQ(eim) {
-        eim.stopEventTimer();
-        eim.setEventCleared();
-        
-        eim.warpEventTeam(910010100);
+  eim.stopEventTimer();
+  eim.setEventCleared();
+
+  eim.warpEventTeam(910010100);
 }
 
 function monsterKilled(mob, eim) {}
 
 function friendlyKilled(mob, eim) {
-        if (mob.getId() == 9300061) {
-                eim.schedule("bunnyDefeated", 5 * 1000);
-        }
+  if (mob.getId() == 9300061) {
+    eim.schedule("bunnyDefeated", 5 * 1000);
+  }
 }
 
 function friendlyItemDrop(eim, mob) {
-        if (mob.getId() == 9300061) {
-                var cakes = eim.getIntProperty("bunnyCake") + 1;
-                eim.setIntProperty("bunnyCake", cakes);
-                
-                mob.getMap().broadcastMessage(Packages.tools.MaplePacketCreator.serverNotice(6, "The Moon Bunny made rice cake number " + cakes + "."));
-        }
+  if (mob.getId() == 9300061) {
+    var cakes = eim.getIntProperty("bunnyCake") + 1;
+    eim.setIntProperty("bunnyCake", cakes);
+
+    mob
+      .getMap()
+      .broadcastMessage(
+        Packages.tools.MaplePacketCreator.serverNotice(
+          6,
+          "The Moon Bunny made rice cake number " + cakes + "."
+        )
+      );
+  }
 }
 
 function friendlyDamaged(eim, mob) {
-        if (mob.getId() == 9300061) {
-                var bunnyDamage = eim.getIntProperty("bunnyDamaged") + 1;
-                if (bunnyDamage > 5) {
-                        broadcastMessage(Packages.tools.MaplePacketCreator.serverNotice(6, "The Moon Bunny is feeling sick. Please protect it so it can make delicious rice cakes."));
-                        eim.setIntProperty("bunnyDamaged", 0);
-                }
-        }
+  if (mob.getId() == 9300061) {
+    var bunnyDamage = eim.getIntProperty("bunnyDamaged") + 1;
+    if (bunnyDamage > 5) {
+      broadcastMessage(
+        Packages.tools.MaplePacketCreator.serverNotice(
+          6,
+          "The Moon Bunny is feeling sick. Please protect it so it can make delicious rice cakes."
+        )
+      );
+      eim.setIntProperty("bunnyDamaged", 0);
+    }
+  }
 }
 
 function allMonstersDead(eim) {}
@@ -269,4 +291,3 @@ function allMonstersDead(eim) {}
 function cancelSchedule() {}
 
 function dispose(eim) {}
-
