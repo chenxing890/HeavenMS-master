d4400db436e1ba15f0a0da8daeed2ca1660e6111
Richard Wu <razor.wu1994@gmail.com>
2020-05-18 11:31:16 -0400

    Update 1002002.js

--- 剧本/全国人大/1002002.js
+++ 剧本/全国人大/1002002.js
@@ -27,7 +27,7 @@
 var status = 0;
 
 function start() {
-    cm.sendSimple("您是否听说过位于利斯港附近，享有#bFlorina海滩#k壮观海景的海滩？ 我现在可以带您去＃b1500 mesos＃k，或者如果您有#bVIP到Florina Beach＃k的票，在这种情况下，您将免费在那里。\ r \ n \ r \ n ＃L0 ## b我将支付1500 Mesos。＃l \ r \ n＃L1＃我有一张前往弗洛里纳海滩的VIP门票。＃l \ r \ n＃L2＃什么是到弗洛里纳海滩的VIP门票#k？ #l");
+    cm.sendSimple("您是否听说过位于利斯港附近，享有#b黄金海岸#k壮观海景的海滩？ 我现在可以带您去＃b1500 金币＃k，或者如果您有#bVIP到黄金海岸＃k的票，在这种情况下，您将免费在那里。\ r \ n \ r \ n ＃L0 ## b我将支付1500 金币。＃l \ r \ n＃L1＃我有一张前往黄金海岸的VIP门票。＃l \ r \ n＃L2＃什么是到黄金海岸的VIP门票#k？ #l");
 }
 
 function action(mode, type, selection) {
@@ -44,18 +44,18 @@ function action(mode, type, selection) {
         status++;
     if(status == 1){
         if(selection == 1)
-            cm.sendYesNo("那么，您有#bVIP到Florina Beach＃k的票吗？ 您可以随时随地前往弗洛里纳海滩。 好的，但是请注意，您可能也在那里遇到了一些怪物。 好的，您现在想去佛罗里达海滩吗？");
+            cm.sendYesNo("那么，您有#bVIP到黄金海岸＃k的票吗？ 您可以随时随地前往黄金海岸。 好的，但是请注意，您可能也在那里遇到了一些怪物。 好的，您现在想去黄金海岸吗？");
         else if (selection == 2)
-            cm.sendNext("您必须对#bVIP佛罗里达海滩#k票感到好奇。 哈哈，这是很容易理解的。 弗洛里纳海滩的VIP票是一项只要您拥有的物品，您就可以免费前往弗洛里纳海滩。 这种稀有物品甚至连我们都必须买下来，但是不幸的是，几个星期前，在我宝贵的暑假期间，我迷失了我。");
+            cm.sendNext("您必须对#bVIP黄金海岸#k票感到好奇。 哈哈，这是很容易理解的。 黄金海岸的VIP票是一项只要您拥有的物品，您就可以免费前往黄金海岸。 这种稀有物品甚至连我们都必须买下来，但是不幸的是，几个星期前，在我宝贵的暑假期间，我迷失了我。");
     } else if (status == 2){
         if(type != 1 && selection != 0) {
             cm.sendNextPrev("我没有它就回来了，没有它简直太可怕了。 希望有人把它捡起来放在安全的地方。 无论如何，这是我的故事，谁知道，您也许可以捡起它并加以利用。 如果你有任何问题随时问。.");
 			cm.dispose();
 		} else{
             if (cm.getMeso() < 1500 && selection == 0)
-                cm.sendNext("我认为您缺少介意。 您知道有很多方法来筹集资金，例如……卖掉盔甲，打败怪物……执行任务……您知道我在说什么。");
+                cm.sendNext("我认为您缺少金币。 您知道有很多方法来筹集资金，例如……卖掉盔甲，打败怪物……执行任务……您知道我在说什么。");
             else if(!cm.haveItem(4031134) && selection != 0){
-                cm.sendNext("嗯，那么您到Florina \ r \ nBeach＃k的#bVIP票到底在哪里？ 您确定有一个吗？ 请仔细检查。");
+                cm.sendNext("嗯，那么您到黄金 \ r \ n海岸＃k的#bVIP票到底在哪里？ 您确定有一个吗？ 请仔细检查。");
             }else{
                 if(selection == 0)
                     cm.gainMeso(-1500);
