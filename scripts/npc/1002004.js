a7fa3a8cbc419a705db9bb64d7d6bdb4c8598689
Richard Wu <razor.wu1994@gmail.com>
2020-05-19 10:02:48 -0400

    Update 1002004.js

--- scripts/npc/1002004.js
+++ scripts/npc/1002004.js
@@ -33,7 +33,7 @@ var status = 0;
 var cost = 10000;
 
 function start() {
-    cm.sendNext("嗨，您好！ 此出租车仅适用于VIP客户。 我们不仅会像普通出租车一样带您前往其他城镇，我们还提供了值得VIP级别的更好服务。 它有点贵，但是...仅需10,000个messos，我们将带您安全前往\ r \ n＃bAnt Tunnel＃k。");
+    cm.sendNext("嗨，您好！ 此出租车仅适用于VIP客户。 我们不仅会像普通出租车一样带您前往其他城镇，我们还提供了值得VIP级别的更好服务。 它有点贵，但是...仅需10,000个messos，我们将带您安全前往\ r \ n＃b蚂蚁隧道＃k。");
 }
 
 function action(mode, type, selection) {
@@ -47,7 +47,7 @@ function action(mode, type, selection) {
     	return;
     }
     if (status == 1) {
-        cm.sendYesNo(cm.getJobId() == 0 ? "我们为初学者提供90％的特别折扣。 蚂蚁隧道位于地牢的深处，地牢位于维多利亚岛的中心，那里是24小时流动商店所在的地方。 您想以＃b1,000 mesos＃k的价格去那儿吗？“：”常规费用适用于所有非初学者。 蚂蚁隧道位于地牢内部的深处，地牢位于维多利亚岛的中心，那里是24小时流动商店所在的地方。 您想去那里＃b10,000 mesos＃k吗？");
+        cm.sendYesNo(cm.getJobId() == 0 ? "我们为初学者提供90％的特别折扣。 蚂蚁隧道位于地牢的深处，地牢位于维多利亚岛的中心，那里是24小时流动商店所在的地方。 您想以＃b1,000 mesos＃k的价格去那儿吗？“：”常规费用适用于所有非初学者。 蚂蚁隧道位于地牢内部的深处，地牢位于维多利亚岛的中心，那里是24小时流动商店所在的地方。 您想花＃b10,000 mesos＃k去那里吗？");
         cost /= ((cm.getJobId() == 0) ? 10 : 1);
     } else if (status == 2) {
         if (cm.getMeso() < cost)
